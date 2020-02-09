50964d71885345a258a1b186b516b447155b02ee
Sasho <counter080@gmail.com>
2020-02-09 12:47:34 +0200

    Initial Commit

---
+++ TestingExercise/HeroesGame/Implementation/Hero/Mage.cs
@@ -0,0 +1,29 @@
+﻿using HeroesGame.Constant;
+using HeroesGame.Implementation.Weapon;
+using System;
+
+namespace HeroesGame.Implementation.Hero
+{
+    public class Mage : BaseHero
+    {
+        public Mage() : base(new Staff())
+        {
+        }
+
+        protected override void LevelUp()
+        {
+            this.Level++;
+
+            this.Armor += HeroConstants.MageArmorPerLevel;
+            this.MaxHealth += HeroConstants.MageMaxHealthPerLevel;
+            this.Health = this.MaxHealth;
+
+            this.Weapon.LevelUp();
+        }
+
+        public static implicit operator global::Moq.Mock<object>(Mage v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
